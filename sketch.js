8e2c4bd4777c7aa41eff19bfcab26a2124b9813d
Zairah11 <110927002+Zairah11@users.noreply.github.com>
2022-08-16 12:35:32 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(400, 400);
+}
+//var ball = createSprite (200,200,10,10)
+function draw() {
+  background ("yellow");
+  if(keyIsDown(RIGHT_ARROW)){
+background ("purple")}}
